a67358367b45af6cebfcf34fa8a415a84c44e376
wuyi3 <wuyi3@xiaomi.com>
2022-03-21 21:43:58 +0800

    add

---
+++ src/test/java/com/wd/xuanke/XuankeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wd.xuanke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XuankeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
